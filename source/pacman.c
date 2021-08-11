7f57a18120df1b9bf6ce890666695c71133f29f5
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-08-11 19:09:15 -0300

    Move pointer

--- source/pacman.c
+++ source/pacman.c
@@ -9,11 +9,19 @@
 
 typedef struct Player {
 
-	ALLEGRO_COLOR color;
-	int x, y, size;
 	float hp;
+	int x, y, size;
+	ALLEGRO_COLOR color;
 
 } Player;
+
+typedef struct Pointer {
+
+	int option;
+	int x, y, size;
+	ALLEGRO_COLOR color;
+
+} Pointer;
 
 //variaveis globais
 const float FPS = 100;
@@ -186,7 +194,7 @@ float dist(int x1, int y1, int x2, int y2){
 
 bool foundGhost(Player p){
 
-	return false;
+	return true;
 
 	// if(dist(p.x, p.y, 0, 0) <= MAX_DIST){
 	// 	return true;
@@ -226,12 +234,24 @@ void initBattlePlayer(Player *p){
 
 }
 
+void initPointer(Pointer *pointer){
+	int constant = (SCREEN_W / 2) - 340;
+	pointer->size = 5;
+	pointer->option = 1;
+	pointer->x = constant + (constant / 2) + (pointer->size * 3);
+	pointer->y = MO_Y + 32;
+	pointer->color = al_map_rgb(4, 230, 0);
+
+	// al_draw_filled_circle(first_pointer_x, (MO_Y + 32), pointer_size, al_map_rgb(4, 230, 0));
+	// al_draw_filled_circle(middle_pointer_x, (MO_Y + 32), pointer_size, al_map_rgb(4, 230, 0));
+	// al_draw_filled_circle(third_pointer_x, (MO_Y + 32), pointer_size, al_map_rgb(4, 230, 0));
+}
 
 void drawBattleScenario(Player p){
 	int shadow_y = MO_Y + 1;
-	int middle_x = (SCREEN_W / 2) - 140;
-	int first_x = middle_x - 200;
-	int third_x = middle_x + 360;
+	int middle_option_x = (SCREEN_W / 2) - 140;
+	int first_option_x = middle_option_x - 200;
+	int third_option_x = middle_option_x + 360;
 
 
 	// DISPLAY AND MENU ----------------------------------------------------------------------------------------------
@@ -241,17 +261,93 @@ void drawBattleScenario(Player p){
 	al_draw_filled_rectangle(0, (MENU_SIZE + 5), SCREEN_W, (MENU_SIZE + 7), al_map_rgb(9, 0, 255));
 
 	// MENU OPTIONS ----------------------------------------------------------------------------------------------
-	al_draw_text(font, al_map_rgb(143, 16, 7), (first_x + 1), shadow_y, 0, "ATTACK"); //shadow 
-	al_draw_text(font, al_map_rgb(9, 0, 255), first_x, MO_Y, 0, "ATTACK");
+	al_draw_text(font, al_map_rgb(143, 16, 7), (first_option_x + 1), shadow_y, 0, "ATTACK"); //shadow 
+	al_draw_text(font, al_map_rgb(9, 0, 255), first_option_x, MO_Y, 0, "ATTACK");
 
-	al_draw_text(font, al_map_rgb(143, 16, 7), (middle_x + 1), shadow_y, 0, "SPECIAL ATTACK"); //shadow 
-	al_draw_text(font, al_map_rgb(9, 0, 255), middle_x, MO_Y, 0, "SPECIAL ATTACK");
+	al_draw_text(font, al_map_rgb(143, 16, 7), (middle_option_x + 1), shadow_y, 0, "SPECIAL ATTACK"); //shadow 
+	al_draw_text(font, al_map_rgb(9, 0, 255), middle_option_x, MO_Y, 0, "SPECIAL ATTACK");
 
-	al_draw_text(font, al_map_rgb(143, 16, 7), (third_x + 1), shadow_y, 0, "RUN"); //shadow 
-	al_draw_text(font, al_map_rgb(9, 0, 255), third_x, MO_Y, 0, "RUN");
-	
-	// POINTER -----------------------------------------------------------------------------------------------------------
+	al_draw_text(font, al_map_rgb(143, 16, 7), (third_option_x + 1), shadow_y, 0, "RUN"); //shadow 
+	al_draw_text(font, al_map_rgb(9, 0, 255), third_option_x, MO_Y, 0, "RUN");
 
+}
+
+void drawPointer(Pointer p){
+	al_draw_filled_circle(p.x, p.y, p.size, p.color);
+}
+
+void battleKeyDown(Pointer *pointer, int key){
+	// int constant = (SCREEN_W / 2) - 340;
+	// int x = constant + (constant / 2) + (p->size * 3);
+	int constant = (SCREEN_W / 2) - 140;
+
+	int constant2 = constant - 200;
+	int option1 = constant2 + (constant2 / 2) + (pointer->size * 3);
+	int option2 = (SCREEN_W / 2) - (pointer->size * 3);
+	int option3 = option2 + 260;
+
+	// int middle_pointer_x = (SCREEN_W / 2) - (pointer_size * 3);
+	// int first_pointer_x = first_option_x + (first_option_x / 2) + (pointer_size * 3);
+	// int third_pointer_x =  third_option_x + (pointer_size * 5);
+
+	switch(key){
+		case ALLEGRO_KEY_LEFT:
+			if(pointer->option == 1){
+				pointer->x = option3;
+				pointer->option = 3;
+				break;
+			}
+
+			if(pointer->option == 2){
+				pointer->x = option1;
+				pointer->option = 1;
+				break;
+			}
+
+			if(pointer->option == 3){
+				pointer->x = option2;
+				pointer->option = 2;
+				break;
+			}
+		break;
+
+		case ALLEGRO_KEY_RIGHT:
+			if(pointer->option == 1){
+				pointer->x = option2;
+				pointer->option = 2;
+				break;
+			}
+
+			if(pointer->option == 2){
+				pointer->x = option3;
+				pointer->option = 3;
+				break;
+			}
+
+			if(pointer->option == 3){
+				pointer->x = option1;
+				pointer->option = 1;
+				break;
+			}
+		break;
+
+		case ALLEGRO_KEY_ENTER:
+			if(pointer->option == 1){
+				//code
+				break;
+			}
+
+			if(pointer->option == 2){
+				//code
+				break;
+			}
+
+			if(pointer->option == 3){
+				//code
+				break;
+			}
+		break;
+	}
 
 }
 
@@ -261,12 +357,14 @@ int main(int argc, char const *argv[]){
 
 	init();
 	Player p; 
+	Pointer pointer; 
 	bool playing = true;
 	bool exploration = true; //true para exploration, false para fight
 
 	// LOOPING DO JOGO --------------------------------------------------------------------------------------------------------
 	al_start_timer(timer);
 	initExplorationPlayer(&p);
+	initPointer(&pointer);
 	
 	while(playing){
 
@@ -291,6 +389,7 @@ int main(int argc, char const *argv[]){
 
 				initBattlePlayer(&p);
 				drawBattleScenario(p);
+				drawPointer(pointer);
 				drawBattlePlayer(p);
 
 			}
@@ -316,6 +415,9 @@ int main(int argc, char const *argv[]){
 				// printf(", ");
 				// printf("%i", p.y);
 				// printf("\n");
+			} else {
+
+				battleKeyDown(&pointer, ev.keyboard.keycode);
 			}
 
 
